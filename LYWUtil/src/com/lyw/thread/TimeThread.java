bb28869a7f1035e230d678aad62f4bcc44134f44
281451241 <281451241@qq.com>
2013-05-10 02:25:22 -0700

    用javax.swing.Timer实现的限时线程

--- LYWUtil/src/com/lyw/thread/TimeThread.java
+++ LYWUtil/src/com/lyw/thread/TimeThread.java
@@ -50,16 +50,17 @@ class Threads extends Thread
 	}
 }
 
-class TestThreads extends Thread {
-	
-	Timer timer;  //定时器
-	
+class TestThreads extends Thread
+{
+
+	Timer timer; // 定时器
+
 	public TestThreads()
 	{
-		timer = new Timer(5000, null);	//定时5秒
+		timer = new Timer(5000, null); // 定时5秒
 		timer.setRepeats(false);
 	}
-	
+
 	@Override
 	public void run()
 	{
@@ -77,11 +78,89 @@ class TestThreads extends Thread {
 	}
 }
 
+class UtilTimer extends Thread
+{
+	Thread timer;
+
+	UtilTimer()
+	{
+		timer = new Thread()
+		{
+			@Override
+			public void run()
+			{
+				try
+				{
+					Thread.sleep(2000);
+				}
+				catch (InterruptedException e)
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		};
+	}
+
+	@Override
+	public void run()
+	{
+		long cur = System.currentTimeMillis();
+		timer.start();
+		while (true)
+		{
+			if (!timer.isAlive())
+			{
+				System.out.println("End");
+				break;
+			}
+		}
+		System.out.println(System.currentTimeMillis() - cur);
+	}
+}
+
 public class TimeThread
 {
+	private void tTime()
+	{
+		Thread timer = new Thread()
+		{
+			@Override
+			public void run()
+			{
+				try
+				{
+					Thread.sleep(2000);
+				}
+				catch (InterruptedException e)
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		};
+
+		timer.start();
+		long cur = System.currentTimeMillis();
+		while (timer.isAlive())
+		{
+		}
+		System.out.println(System.currentTimeMillis() - cur);
+		System.out.println("End");
+	}
+	
+	private void f() {
+		long cur = System.currentTimeMillis();
+		while (System.currentTimeMillis() - cur < 2000)
+		{
+			System.out.println(System.currentTimeMillis() - cur);
+		}
+		System.out.println("end");
+	}
+
 	public static void main(String args[])
 	{
-		TestThreads t = new TestThreads();
-		t.start();
+		TimeThread t = new TimeThread();
+		t.f();
 	}
 }
