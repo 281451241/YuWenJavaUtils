cb6e1bb9358ffed0d72df386ef2674073cfb0653
281451241 <281451241@qq.com>
2013-07-19 00:21:45 -0700

    增加打印数组方法

--- LYWUtil/src/com/util/P.java
+++ LYWUtil/src/com/util/P.java
@@ -6,6 +6,10 @@ public class P
 	{
 		System.out.println(str);
 	}
+	public static void p(String str)
+	{
+		System.out.print(str);
+	}
 
 	public static void print(int i)
 	{
@@ -21,4 +25,77 @@ public class P
 	{
 		System.out.println(b);
 	}
+	
+	/**
+	 * 打印数组并换行
+	 * @param str
+	 */
+	public static void print(String[] str) {
+		for(String s : str) {
+			p(s);
+		}
+		print("");
+	}
+	
+	/**
+	 * 打印数组并以sep分隔
+	 * @param str
+	 * @param sep
+	 */
+	public static void print(String[] str, String sep) {
+		
+		for(int i=0,n=str.length; i<n; i++) {
+			p(str[i]);
+			if(i == n-1)
+				break;
+			p(sep);
+		}
+		print("");
+	}
+	
+	
+	/**
+	 * 打印数组并以sep/startStr/endStr 分隔/开头/结尾
+	 * @param str
+	 * @param sep
+	 * @param startStr
+	 * @param endStr
+	 */
+	public static void print(String[] str, String sep, String startStr, String endStr) {
+		p(startStr);
+		for(int i=0,n=str.length; i<n; i++) {
+			p(str[i]);
+			if(i == n-1)
+				break;
+			p(sep);
+		}
+		p(endStr);
+		print("");
+	}
+	
+	/**
+	 * 打印数组并以sep/startStr/endStr 分隔/开头/结尾
+	 * 为每个元素前后加上roundStart/roundEnd
+	 * @param str
+	 * @param sep
+	 * @param startStr
+	 * @param endStr
+	 * @param roundStart
+	 * @param roundEnd
+	 */
+	public static void printSurroundBy(String[] str, String sep,
+			String startStr, String endStr,
+			String roundStart, String roundEnd) {
+		p(startStr);
+		for(int i=0,n=str.length; i<n; i++) {
+			p(roundStart+str[i]+roundEnd);
+			if(i == n-1)
+				break;
+			p(sep);
+		}
+		p(endStr);
+		print("");
+	}
+	
+	
 }
