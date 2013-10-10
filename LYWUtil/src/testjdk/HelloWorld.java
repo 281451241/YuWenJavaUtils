96197d0d03bb33100b88574279ccfaa25a6f78a4
281451241 <281451241@qq.com>
2013-10-10 17:35:45 +0800

    冲突了

--- LYWUtil/src/testjdk/HelloWorld.java
+++ LYWUtil/src/testjdk/HelloWorld.java
@@ -5,6 +5,6 @@ public class HelloWorld
 	public static void main(String[] args)
 	{
 		System.out.println("HelloWorld");
-		System.out.println("HelloWorld");
+		System.out.println(1 ^ 1 + 1);
 	}
 }
