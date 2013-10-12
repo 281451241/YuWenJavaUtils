e0c3b585b57d651641ca1652ac0eefa067ff7e59
281451241 <281451241@qq.com>
2013-10-12 09:35:40 +0800

    增加OGNL测试代码

---
+++ LYWUtil/src/com/lyw/web/ognl/OGNLTestCase.java
@@ -0,0 +1,64 @@
+package com.lyw.web.ognl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import junit.framework.TestCase;
+import ognl.Ognl;
+
+import org.junit.Test;
+
+public class OGNLTestCase extends TestCase {  
+      
+    /** 
+     *  
+     * @throws Exception 
+     */  
+    @SuppressWarnings("unchecked")  
+    @Test  
+    public void testGetValue() throws Exception {  
+          
+        // Create root object  
+        User user = new User();  
+        user.setId(1);  
+        user.setName("downpour");  
+  
+        // Create context  
+        Map context = new HashMap();  
+        context.put("introduction","My name is ");  
+          
+        // Test to directly get value from root object, with no context  
+        Object name = Ognl.getValue(Ognl.parseExpression("name"), user);  
+        assertEquals("downpour",name);  
+          
+        // Test to get value(parameter) from context  
+        Object contextValue = Ognl.getValue(Ognl.parseExpression("#introduction"), context, user);  
+        assertEquals("My name is ", contextValue);  
+          
+        // Test to get value and parameter from root object and context  
+        Object hello = Ognl.getValue(Ognl.parseExpression("#introduction + name"), context, user);  
+        assertEquals("My name is downpour",hello);  
+                      
+    }  
+  
+    /** 
+     *  
+     * @throws Exception 
+     */  
+    @SuppressWarnings("unchecked")  
+    @Test  
+    public void testSetValue() throws Exception {  
+          
+        // Create root object  
+        User user = new User();  
+        user.setId(1);  
+        user.setName("downpour");  
+          
+                // Set value according to the expression  
+        Ognl.setValue("department.name", user, "dev");  
+        assertEquals("dev", user.getDepartment().getName());  
+          
+    }  
+      
+  
+}  
