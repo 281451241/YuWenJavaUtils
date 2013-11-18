efeef718d42619aea1ab1b6bf115798274468e01
281451241 <281451241@qq.com>
2013-11-18 09:00:55 +0800

    增加相关测试

--- LYWUtil/src/com/lyw/util/TestMap.java
+++ LYWUtil/src/com/lyw/util/TestMap.java
@@ -1,6 +1,7 @@
 package com.lyw.util;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
 
 public class TestMap {
@@ -20,8 +21,43 @@ public class TestMap {
 	}
 	public static void main(String[] args) {
 		TestMap tm = new TestMap();
-		String error = (String) tm.test(true).get("error");
-		System.out.println(tm.test(true).get("success"));
-		System.out.println((String) tm.test(true).get("error") == "1");
+		LinkedHashMap lhMap = tm.new LinkedHashMap();
+		
+		lhMap.put("1", "value");
+		lhMap.put("1", "value2");
+		
+		System.out.println(lhMap.get("1"));
+		System.out.println(lhMap.remove("1"));
+		System.out.println(lhMap.list.size());
+//		String error = (String) tm.test(true).get("error");
+//		System.out.println(tm.test(true).get("success"));
+//		System.out.println((String) tm.test(true).get("error") == "1");
+	}
+	
+	class LinkedHashMap<K, V> extends HashMap<K, V> {
+		
+		LinkedList<K> list;
+		
+		public LinkedHashMap() {
+			list = new LinkedList<K>();
+		}
+		
+		@Override
+		public V put(K key, V value) {
+			V v = super.put(key, value);
+			if(v == value)
+				list.add(key);
+			return v;
+		}
+		
+		@Override
+		public V remove(Object key) {
+			// TODO Auto-generated method stub
+			V value = super.get(key);
+			V v = super.remove(key);
+			if(v == value)
+				list.remove(key);
+			return v;
+		}
 	}
 }
--- LYWUtil/src/com/lyw/util/TestReflect.java
+++ LYWUtil/src/com/lyw/util/TestReflect.java
@@ -9,7 +9,7 @@ import com.lyw.db.mybatis.User;
 public class TestReflect {
 	public void loadClass() throws Throwable {
 		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-		Class clazz = classLoader.loadClass("pojo.Users");	
+		Class clazz = classLoader.loadClass("User");	
 		
 		Constructor constructor = clazz.getDeclaredConstructor((Class[])null);
 		User users = (User) constructor.newInstance();
